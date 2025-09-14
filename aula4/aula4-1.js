fd8127095427bbb528373db8cd524624940fc16a
Eduarda Matos <164113930+dudamatoss@users.noreply.github.com>
2025-09-13 22:30:20 -0300

    aulas

---
+++ aula4/aula4-1.js
@@ -0,0 +1,19 @@
+// let atividade = [
+//     ['trabelho', 8],
+//     ['refeicoes', 2],
+//     ['estudos', 4],
+//     ['dormir', 7]
+// ];
+
+// let somaHoras = 0 ;
+// const HORAS_DO_DIA = 24;
+
+// atividade.forEach(linha => {
+//     console.log(linha);
+//     somaHoras = somaHoras + linha[1];
+// });
+
+// let tempoLivre = HORAS_DO_DIA - somaHoras;
+// console.log("A sua rotina tem " + somaHoras + " horas");
+// console.log("Ainda sobra " + tempoLivre.toFixed(2) + " horas de tempo livre");
+
